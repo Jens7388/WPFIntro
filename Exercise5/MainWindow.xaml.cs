94af1637f3ffbda2555f096f1692cdc1f8d7eca3
Jens Zakarias Pedersen <jens7388@edu.campusvejle.dk>
2020-01-20 14:16:36 +0100

    Added a square that moves each time you click on it.

---
+++ Exercise5/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercise5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow: Window
+    {
+        int boxPositionLeft;
+        int boxPositionTop;
+        int boxPositionRight;
+        int boxPositionBottom;
+        Random positionGenerator = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {        
+            boxPositionLeft = positionGenerator.Next(10, 790);
+            boxPositionTop = positionGenerator.Next(10, 790);
+            boxPositionRight = positionGenerator.Next(10, 790 - boxPositionLeft);
+            boxPositionBottom = positionGenerator.Next(10, 790 - boxPositionTop);
+            clickMe.Margin = new Thickness(boxPositionLeft, boxPositionTop, boxPositionRight, boxPositionBottom);
+        }
+    }
+}
